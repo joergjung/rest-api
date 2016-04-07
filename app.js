e357188dd951d887234a61b1bb5e4a2d59b030ba
Joerg Jung <joerg-jung@web.de>
2016-04-08 01:15:35 +0200

    add bookRouter

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+    .get(function(req, res) {
+        var responseJson = {hello: 'Hello fom API!'};
+        res.json(responseJson);
+    });
+
+app.use('/api', bookRouter);
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
