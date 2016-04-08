e1e4c361d423cf04736cf243e7f3483a82eb6d26
Joerg Jung <joerg-jung@web.de>
2016-04-08 12:45:05 +0200

    add bookModel and adminRoutes

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+// connect to database
+var db = mongoose.connect('mongodb://localhost:27017/bookAPI');
+var Book = require('./models/bookModel');
 var port = process.env.PORT || 3000;
+var adminRouter = require('./routes/adminRoutes')();
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
     .get(function(req, res) {
-        var responseJson = {hello: 'Hello fom API!'};
-        res.json(responseJson);
+        Book.find(function(err, books) {
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(books);
+            }
+        });
     });
 
 app.use('/api', bookRouter);
+app.use('/Admin', adminRouter);
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
