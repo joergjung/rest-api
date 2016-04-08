5f4dff2a7c2616e7ec576a6f4f6918c8fd94c659
Joerg Jung <joerg-jung@web.de>
2016-04-08 13:42:29 +0200

    add filtering only by genre

--- app.js
+++ app.js
@@ -9,8 +9,18 @@ var adminRouter = require('./routes/adminRoutes')();
 var bookRouter = express.Router();
 
 bookRouter.route('/Books')
-    .get(function(req, res) {
-        Book.find(function(err, books) {
+    .get(function(req, res) {     
+        // req.query returns a JSON object which contains the query parameters
+        // for example with http://localhost:8000/api/Books?author=Victor Hugo
+        // req.query will return this JSON object: { author: 'Victor Hugo' }
+        // adding it as a parameter into Book.find will filter automatically
+        var query = {};
+
+        // aviod random user queries and allow only filtering by genre
+        if (req.query.genre) {
+            query.genre = req.query.genre;
+        }
+        Book.find(query, function(err, books) {            
             if (err) {
                 res.status(500).send(err);
             } else {
